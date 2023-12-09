649eb99d4a22eacd8d24438f3547e1b0a7504222
Nguyen Huyen <nguyenhuyen@MacBook-Pro-cua-Nguyen.local>
2023-12-09 11:28:43 -0800

    add files

--- calculator.js
+++ calculator.js
@@ -1,10 +1,53 @@
-const calcBtns = document.querySelectorAll("button");
+const numBtns = document.querySelectorAll(".showNumber");
+const calcBtns = document.querySelectorAll('.operation')
+const clear = document.getElementById('clear');
 let output = document.getElementById('outputBox');
+let isFirst = true; // for the first input number
+let isFirst2 = true; //for the second input number
+let isClicked = true; // for the operation 
+let firstNumber = 0;
+let secondNumber = 0;
 
-function show(event){
+function showNum(event){
     let clickedBtn = event.target;
-    output.innerText += clickedBtn.innerText;
+    
+    //if the first number input, it removes the default 0
+    //else keeping adding on to the output
+    if(isFirst) output.innerText = clickedBtn.innerText;
+    else output.innerText += clickedBtn.innerText;
+    firstNumber = parseFloat(output);
+    isFirst = false;
+
+    calcBtns.forEach((calc) => {
+        calc.onclick = function(){
+            output.innerText = 0;
+        }
+    }); 
+
+    if(isClicked){
+        if(isFirst2) output.innerText = clickedBtn.innerText;
+        else output.innerText += clickedBtn.innerText;
+        secondNumber = parseFloat(output);
+        isFirst2 = false;
+    }
+
+    // if(clickedBtn.innerText === '+') firstNumber += secondNumber;
+    // else if(clickedBtn.innerText === '-') firstNumber -= secondNumber;
+    // else if(clickedBtn.innerText === '/') firstNumber /= secondNumber;
+    // else if(clickedBtn.innerText === '*') firstNumber *= secondNumber;
+    // isClicked = false;
+
 }
-calcBtns.forEach((button => {
-    button.addEventListener('click', show);
+
+
+
+numBtns.forEach((button => {
+    button.addEventListener('click', showNum);
 }));
+
+
+clear.addEventListener('click', () => {
+    output.innerText = 0;
+    firstNumber = 0;
+    secondNumber = 0;
+});
