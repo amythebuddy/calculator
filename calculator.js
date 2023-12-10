3f3fc999e32ee1f27c1f5ca32876352f634b88e3
Nguyen Huyen <nguyenhuyen@MacBook-Pro-cua-Nguyen.local>
2023-12-09 20:57:52 -0800

    add files

--- calculator.js
+++ calculator.js
@@ -1,53 +1,66 @@
 const numBtns = document.querySelectorAll(".showNumber");
 const calcBtns = document.querySelectorAll('.operation')
 const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
 let output = document.getElementById('outputBox');
-let isFirst = true; // for the first input number
-let isFirst2 = true; //for the second input number
-let isClicked = true; // for the operation 
-let firstNumber = 0;
-let secondNumber = 0;
+let isFirstDigit = true; // for the first input digit
+let isFirstDigit2 = true; // for the first input digit
+let isClicked = false; // for the operation 
+let placeholder = "";
+let amountOfCalculation = 0;
 
 function showNum(event){
     let clickedBtn = event.target;
     
-    //if the first number input, it removes the default 0
-    //else keeping adding on to the output
-    if(isFirst) output.innerText = clickedBtn.innerText;
-    else output.innerText += clickedBtn.innerText;
-    firstNumber = parseFloat(output);
-    isFirst = false;
+    // if the first digit input, it removes the default 0
+    // else keeping adding on to the output
+    if(isFirstDigit) {
+        output.innerText = clickedBtn.innerText;
+    } else if(isClicked){
+        if(isFirstDigit2) {
+            output.innerText = clickedBtn.innerText;
+        } else {
+            output.innerText += clickedBtn.innerText;
+        }
+        placeholder += clickedBtn.innerText;
+        isFirstDigit2 = false;
+    } else{ 
+        output.innerText += clickedBtn.innerText;
+        placeholder = output.innerText;
+    }
+    isFirstDigit = false;
 
     calcBtns.forEach((calc) => {
-        calc.onclick = function(){
+        calc.onclick = function(event){
+            let calcButton = event.target;
+            amountOfCalculation++;
+            if(calcButton.innerText === '*' || calcButton.innerText === '/'){
+                placeholder = "(" + placeholder + ")";
+            }
+            placeholder += calcButton.innerText;
             output.innerText = 0;
+            isClicked = true;
+            if(amountOfCalculation > 1) {
+                isFirstDigit2 = true;
+            }
         }
-    }); 
-
-    if(isClicked){
-        if(isFirst2) output.innerText = clickedBtn.innerText;
-        else output.innerText += clickedBtn.innerText;
-        secondNumber = parseFloat(output);
-        isFirst2 = false;
-    }
-
-    // if(clickedBtn.innerText === '+') firstNumber += secondNumber;
-    // else if(clickedBtn.innerText === '-') firstNumber -= secondNumber;
-    // else if(clickedBtn.innerText === '/') firstNumber /= secondNumber;
-    // else if(clickedBtn.innerText === '*') firstNumber *= secondNumber;
-    // isClicked = false;
-
+    });
+    console.log(placeholder);
 }
-
 
 
 numBtns.forEach((button => {
     button.addEventListener('click', showNum);
 }));
 
-
 clear.addEventListener('click', () => {
     output.innerText = 0;
-    firstNumber = 0;
-    secondNumber = 0;
+    isFirstDigit = true;
+    isFirstDigit2 = true;
+    isClicked = false;
+    placeholder = "";
+});
+equal.addEventListener('click', () => {
+    let result = Function("return " + placeholder)();
+    output.innerText = result;
 });
