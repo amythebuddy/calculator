8e3e739d666993645f33dd390075e3198c75433e
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-12 09:58:19 -0800

    add files via terminal

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 const numBtns = document.querySelectorAll(".showNumber");
-const calcBtns = document.querySelectorAll('.operation')
+const calcBtns = document.querySelectorAll('.operation');
 const clear = document.getElementById('clear');
 const equal = document.getElementById('equal');
 const negative = document.getElementById('neg');
-const percent = document.getElementById('percent');
+const factorial = document.getElementById('factorial');
 let output = document.getElementById('outputBox');
 let isFirstDigit = true; // for the first input digit
 let isFirstDigit2 = true; // for the second input digit
@@ -11,9 +11,13 @@ let isNegative = false; // for the negative number
 let isClicked = false; // for the operation 
 let calculation = ""; // store the calculation
 let amountOfCalculation = 0;
+let arrFactorial = [];
 
 function showNum(event){
     let clickedBtn = event.target;
+
+    //Condition : You need to press any number first then press this button for the negative sign to appear
+    // You cannot add the negative sign to the default 0
 
     //if output includes negative, then remove the negative sign
     negative.onclick = function() {
@@ -22,14 +26,15 @@ function showNum(event){
             isNegative = false;
         } else { // else add the negative sign
             output.innerText = '-' + output.innerText;
-            // calculation = output.innerText;
             isNegative = true;
         }
-        console.log("hi");
+        calculation = output.innerText;
     };
 
+
+
     if(isFirstDigit) {// if the first digit input, it removes the default 0
-        output.innerText = clickedBtn.innerText;
+        output.innerText = clickedBtn.innerText; 
         calculation = output.innerText;
     } else if(isClicked){ // for the second number 
         if(isFirstDigit2) { // if it's the first digit, then change the 0 
@@ -39,17 +44,13 @@ function showNum(event){
         }
         calculation += clickedBtn.innerText;
         isFirstDigit2 = false;
-    } else if(isFirstDigit && isNegative){
-        output.innerText = '-' + output.innerText;
-        output.innerText += clickedBtn.innerText;
-        calculation = output.innerText;
-    }  else{ // else keeping adding on to the first number output
+    } else{ // else keeping adding on to the first number output
         output.innerText += clickedBtn.innerText;
         calculation = output.innerText;
     }
-    isFirstDigit = false;
+    isFirstDigit = false; // to stop changing the digit
 
-    calcBtns.forEach((calc) => {
+    calcBtns.forEach((calc) => { // operation
         calc.onclick = function(event){
             let calcButton = event.target;
             amountOfCalculation++;
@@ -72,25 +73,38 @@ function showNum(event){
         isClicked = false;
         calculation = "";
         amountOfCalculation = 0;
-    }
-    console.log(calculation);
+    };
+
+    equal.onclick = function() {
+        let result = Function("return " + calculation)(); // calculate the expression using the Function constructor
+        
+        if(result == undefined){ 
+            alert("You need to input number first before pressing equal!");
+            return;
+        }
+        if(!Number.isInteger(result)) result = result.toFixed(2); //if it is decimal, reduce it back to the nearest 100th
+
+        //if number is divided by 0,  display  error
+        if(Number.isFinite(parseFloat(result))) output.innerText = result;
+        else output.innerText = "ERROR";
+
+    };
+
+    factorial.onclick = function() {
+        output.innerText = calcFactorial(parseFloat(output.innerText)); // display the result of factorial
+        calculation = output.innerText; 
+    };
+
 }
 
-equal.addEventListener('click', () => {
-    let result = Function("return " + calculation)(); // calculate the expression using the Function constructor
-    
-    if(result == undefined){ 
-        alert("You need to input number first before pressing equal!");
-        return;
+function calcFactorial(n) { //using recursive to do factorial
+    if(n < 0) {
+        output.innerText = "ERROR";
     }
-    if(!Number.isInteger(result)) result = result.toFixed(2); //if it is decimal, reduce it back to the nearest 100th
-
-    //if result is divided by 0, output is error
-    if(Number.isFinite(parseFloat(result))) output.innerText = result;
-    else output.innerText = "ERROR";
-});
-
-
+    if (n == 0 || n == 1) return 1;
+    if (arrFactorial[n] > 0) return arrFactorial[n];
+    return arrFactorial[n] = calcFactorial(n-1) * n;
+}
 
 numBtns.forEach(function (button) {
     button.addEventListener('click', showNum); 
